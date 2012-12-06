5bbafd23dbf834a2e7ca79372dc3007b72091ed1
Johandry Amador <johandry@yahoo.com>
2012-12-06 12:16:16 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CMDB::Application.config.secret_token = '00bddf30eb782e2286ae202f6b2f29e73379c994661d5a5d3aa1cfcb9427c4c8a5b669600ff72fa1023a9c7756cad3d18dc04323c63288c2d0824f1f6b5cb5d5'
